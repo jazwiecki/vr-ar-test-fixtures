ff3ae35ff7a58d6bb2db7ef59e89e4c0b6791544
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2019-11-29 17:57:01 -0500

    Android Studio app with empty activity

---
+++ app/src/main/java/edu/colostate/cs/cs567/artestautomationsubject/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.colostate.cs.cs567.artestautomationsubject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
