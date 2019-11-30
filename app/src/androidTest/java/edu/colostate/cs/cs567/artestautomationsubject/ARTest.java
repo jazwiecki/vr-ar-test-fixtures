a0a18fef76c892b9f0f433155453bcd96d5d73b5
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2019-11-29 21:23:13 -0500

    1 working test in emulator

---
+++ app/src/androidTest/java/edu/colostate/cs/cs567/artestautomationsubject/ARTest.java
@@ -0,0 +1,70 @@
+package edu.colostate.cs.cs567.artestautomationsubject;
+
+import android.content.Context;
+import android.content.Intent;
+
+//import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+//import org.junit.runner.RunWith;
+
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.*;
+
+//@RunWith(AndroidJUnit4.class)
+public class ARTest {
+    private static final String TAG = "ARTest_" + MainActivity.class.getSimpleName();
+    private static final String AR_TEST_PACKAGE
+            = "edu.colostate.cs.cs567.artestautiomationsubject";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private UiDevice device;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        device = UiDevice.getInstance(getInstrumentation());
+
+        // Start from the home screen
+        device.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertNotNull(launcherPackage);
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch app under test
+        Context context = getApplicationContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(AR_TEST_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        device.wait(Until.hasObject(By.pkg(AR_TEST_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkPreconditions() {
+        assertNotNull(device);
+    }
+
+    @Test
+    public void checkLabelTextAtStartup() throws UiObjectNotFoundException {
+        UiObject textView = device.findObject(new UiSelector()
+                                                .className("android.widget.TextView"));
+        assertNotNull(textView);
+        if (textView.exists()) {
+            assertEquals("The space in front of you hasn't been processed yet.",
+                    textView.getText());
+        }
+    }
+}
